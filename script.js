48f7093a0f8232e48265d6b7dcb5346947b171d5
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2022-10-16 13:46:27 +0530

    fixes

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () { new TypeIt("#typingaboutme", { strings: ["I am a gaming YouTuber that uploads videos about free fire, and more!"], }).go(); });
+document.addEventListener("DOMContentLoaded", function () { 
+    new TypeIt("#typingaboutme", { 
+        strings: ["I am a gaming YouTuber that uploads videos about free fire, and more!"], 
+    }).go(); 
+});
 
