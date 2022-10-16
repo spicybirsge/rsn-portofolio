989e0567d42e511c9a676b03ac53f078000a9524
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2022-10-16 13:37:06 +0530

    site completed ig

---
+++ script.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded", function () { new TypeIt("#typingaboutme", { strings: ["I am a gaming YouTuber that uploads videos about free fire, and more!"], }).go(); });
+
