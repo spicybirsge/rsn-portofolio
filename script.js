83242abf893936f7bd0d8b61e69b0c3158d795b6
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2022-10-17 13:19:15 +0530

    added error handler

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.addEventListener('DOMContentLoaded', async function() {
     const url = `https://getlatestvideofromchannel.vercel.app/latest_video?channel_id=UCo-Z-aFPvCJ-YCOAmUPBVTw&key=810c76f9d289e2db2ca07c3a5b9298d4d50eb6be221211d1c51e1a968d6696f43b57a53923ff326beb565ca1a0b0e8a1ea206ffac6592af2c03d02be3bf97804afca5d5c7f42bd8d`
-
+try {
     const fetchLV = await fetch(url, {
         method: 'GET'
     })
@@ -14,5 +14,8 @@ document.addEventListener('DOMContentLoaded', async function() {
     const data = await fetchLV.json()
 
     document.getElementById('latest-vid-iframe').src=`https://www.youtube.com/embed/${data.last_video_id}`
+} catch{
+    document.getElementById('latest-vid-iframe').src=`https://www.youtube.com/embed/DnH1xyk9xK0`   
+}
 })
 
